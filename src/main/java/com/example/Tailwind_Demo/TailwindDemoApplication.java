f64e4d564087a3a546419f81ebdf398d835206d0
Ryota-Suzki <ryota.business1211@gmail.com>
2024-12-29 14:09:43 +0900

    初回Commit

---
+++ src/main/java/com/example/Tailwind_Demo/TailwindDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tailwind_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailwindDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailwindDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tailwind_Demo/TailwindDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tailwind_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailwindDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
